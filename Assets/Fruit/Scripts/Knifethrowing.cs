8967381bd6449b06a5603231ff7a22fbb3b8f640
SilverAlfredo <49393621+SilverAlfredo@users.noreply.github.com>
2020-02-05 16:19:07 +0800

    Fruitgame
    Might change music and some audio, still have to tweak score

---
+++ Assets/Fruit/Scripts/Knifethrowing.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knifethrowing : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Vector3 bulletOffset = new Vector3(0, 0.5f, 0);
+    public float player;
+    int bulletLayer;
+
+    public float fireDelay = 0.25f;
+    float cooldownTimer = 0;
+
+    void Start()
+    {
+        bulletLayer = gameObject.layer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player == 1f)
+        {
+            cooldownTimer -= Time.deltaTime;
+
+            if (Input.GetButton("P1M") && cooldownTimer <= 0)
+            {
+                // SHOOT!
+                cooldownTimer = fireDelay;
+
+                Vector3 offset = transform.rotation * bulletOffset;
+                SoundManagerFruit.PlaySound("knife");
+                GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, transform.position + offset, transform.rotation);
+                bulletGO.layer = bulletLayer;
+            }
+        }
+        else if (player == 2f)
+        {
+            cooldownTimer -= Time.deltaTime;
+
+            if (Input.GetButton("P2M") && cooldownTimer <= 0)
+            {
+                // SHOOT!
+                cooldownTimer = fireDelay;
+
+                Vector3 offset = transform.rotation * bulletOffset;
+                SoundManagerFruit.PlaySound("knife");
+                GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, transform.position + offset, transform.rotation);
+                bulletGO.layer = bulletLayer;
+            }
+        }
+        else if (player == 3f)
+        {
+            cooldownTimer -= Time.deltaTime;
+
+            if (Input.GetButton("P3M") && cooldownTimer <= 0)
+            {
+                // SHOOT!
+                cooldownTimer = fireDelay;
+
+                Vector3 offset = transform.rotation * bulletOffset;
+                SoundManagerFruit.PlaySound("knife");
+                GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, transform.position + offset, transform.rotation);
+                bulletGO.layer = bulletLayer;
+            }
+        }
+    }
+}
---
+++ Assets/Fruit/Scripts/SoundManagerFruit.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerFruit : MonoBehaviour
+
+{
+    public static AudioClip knifeSound, pingSound, bombSound, splatSound;
+    static AudioSource audioSrc;
+    public AudioClip knife, ping, bomb, splat;
+
+
+    void Start() {
+        knifeSound = knife;
+        pingSound = ping;
+        bombSound = bomb;
+        splatSound = splat;
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    public static void PlaySound(string clip)
+    {
+        switch (clip)
+        {
+            case "knife":
+                audioSrc.PlayOneShot(knifeSound);
+                break;
+            case "ping":
+                audioSrc.PlayOneShot(pingSound);
+                break;
+            case "bomb":
+                audioSrc.PlayOneShot(bombSound);
+                break;
+            case "splat":
+                audioSrc.PlayOneShot(splatSound);
+                break;
+
+        }
+    }
+
+}
